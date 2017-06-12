3c46edd6f8949fbe3adc3c689b107364a74b06f8
unicroak <unicroak@gmail.com>
2017-06-12 22:34:43 +0900

    [add] 料理の値を追加

--- src/main/java/life/grass/grassitem/tag/CookingTag.java
+++ src/main/java/life/grass/grassitem/tag/CookingTag.java
@@ -1,7 +1,12 @@
 package life.grass.grassitem.tag;
 
 public enum CookingTag implements GrassNBTTag {
-    RESTORE_AMOUNT("RestoreAmount", Integer.class);
+    EXPIRE_DATE("ExpireDate", String.class),
+    RESTORE_AMOUNT("RestoreAmount", Integer.class),
+    ELEMENT_SWEET("ElementSweet", Integer.class),
+    ELEMENT_SPICY("ElementSpicy", Integer.class),
+    ELEMENT_SALTY("ElementSalty", Integer.class),
+    ELEMENT_UMAMI("ElementUmami", Integer.class);
 
     private String key;
     private Class clazz;
@@ -13,7 +18,7 @@ public enum CookingTag implements GrassNBTTag {
 
     @Override
     public String getKey() {
-        return "CookingTag/" + key;
+        return "Cooking/" + key;
     }
 
     @Override
