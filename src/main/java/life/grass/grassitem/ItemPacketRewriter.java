3d9f5da03d0cf1e8ac364facdac841df2d86298e
unicroak <unicroak@gmail.com>
2017-06-18 12:56:08 +0900

    [add] ProtocolLibへの依存を追加

---
+++ src/main/java/life/grass/grassitem/ItemPacketRewriter.java
@@ -0,0 +1,72 @@
+package life.grass.grassitem;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemPacketRewriter {
+    private static ItemPacketRewriter instance;
+
+    static {
+        instance = new ItemPacketRewriter();
+    }
+
+    private ItemPacketRewriter() {
+    }
+
+    public static ItemPacketRewriter getInstance() {
+        return instance;
+    }
+
+
+    public void addListener(ProtocolManager manager, JavaPlugin plugin) {
+        manager.addPacketListener(new PacketAdapter(plugin,
+                ListenerPriority.LOW,
+                PacketType.Play.Server.SET_SLOT,
+                PacketType.Play.Server.WINDOW_ITEMS) {
+
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                PacketType type = event.getPacketType();
+                if (type.equals(PacketType.Play.Server.SET_SLOT)) {
+                    for (ItemStack item : new ItemStack[]{event.getPacket().getItemModifier().read(0)}) {
+                        rewriteItem(item);
+                    }
+                } else if (type.equals(PacketType.Play.Server.WINDOW_ITEMS)) {
+                    for (ItemStack item : event.getPacket().getItemListModifier().read(0)) {
+                        rewriteItem(item);
+                    }
+                }
+            }
+        });
+    }
+
+    private void rewriteItem(ItemStack item) {
+        if (item == null) return;
+        String uniqueName = ItemHandler.findUniqueNameFromItemStack(item).orElse(null);
+        GrassJson json = GrassJson.findGrassJson(uniqueName).orElse(null);
+
+        if (uniqueName == null || json == null) return;
+
+        item.setType(json.getMaterial());
+        item.setDurability(json.getMeta());
+
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', json.getDisplayName()));
+
+        List<String> lore = new ArrayList<>();
+        lore.add(ChatColor.translateAlternateColorCodes('&', json.getDescription()));
+
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+    }
+}
--- src/main/java/life/grass/grassitem/Main.java
+++ src/main/java/life/grass/grassitem/Main.java
@@ -1,14 +1,20 @@
 package life.grass.grassitem;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     private static Main instance;
+    private static ProtocolManager protocolManager;
 
     @Override
     public void onEnable() {
         super.onEnable();
         instance = this;
+
+        protocolManager = ProtocolLibrary.getProtocolManager();
+        ItemPacketRewriter.getInstance().addListener(protocolManager, this);
     }
 
     @Override
