8a0ec847f4083d5848d7441bbaed68f03f518c46
明地えしら <ecilavip@outlook.jp>
2017-07-14 03:30:19 +0900

    [add] getコマンド

--- src/main/java/life/grass/grassitem/GrassItemCommandExecutor.java
+++ src/main/java/life/grass/grassitem/GrassItemCommandExecutor.java
@@ -4,12 +4,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class GrassItemCommandExecutor implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args == null || !sender.isOp() || args.length != 1 || !"grassitem".equalsIgnoreCase(command.getName()))
+        if (args == null || !sender.isOp() || args.length <= 0 || !"grassitem".equalsIgnoreCase(command.getName()))
             return false;
 
         switch (args[0].toUpperCase()) {
@@ -17,6 +18,12 @@ public class GrassItemCommandExecutor implements CommandExecutor {
                 JsonBucket.getInstance().refillBucket();
                 sender.sendMessage(ChatColor.GREEN + "Reloaded.");
                 return true;
+            case "GET":
+                Player player = (Player) sender;
+                if(ItemBuilder.buildByConfigString(args[1]) != null) {
+                    player.getInventory().addItem(ItemBuilder.buildByConfigString(args[1]));
+                    return true;
+                }
             default:
                 return false;
         }
